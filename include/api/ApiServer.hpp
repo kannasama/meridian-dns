07950eaa535f5600461e985e60ce5b6e0e52e869
Michael J. Hill <mjhill@mjhnosekai.com>
2026-03-01 12:13:11 -0500

    feat(api): implement ApiServer with Crow app instance and route wiring

--- include/api/ApiServer.hpp
+++ include/api/ApiServer.hpp
@@ -1,7 +1,27 @@
 #pragma once
 
+#include <crow.h>
+
 #include <memory>
-#include <string>
+
+namespace dns::security {
+class AuthService;
+class CryptoService;
+}  // namespace dns::security
+
+namespace dns::api {
+class AuthMiddleware;
+}
+
+namespace dns::dal {
+class ProviderRepository;
+class ViewRepository;
+class ZoneRepository;
+class VariableRepository;
+class RecordRepository;
+class DeploymentRepository;
+class AuditRepository;
+}  // namespace dns::dal
 
 namespace dns::api {
 
@@ -9,12 +29,34 @@ namespace dns::api {
 /// Class abbreviation: api
 class ApiServer {
  public:
-  ApiServer();
+  ApiServer(dns::security::AuthService& asService,
+            const dns::api::AuthMiddleware& amMiddleware,
+            dns::dal::ProviderRepository& prRepo,
+            dns::dal::ViewRepository& vrRepo,
+            dns::dal::ZoneRepository& zrRepo,
+            dns::dal::VariableRepository& varRepo,
+            dns::dal::RecordRepository& rrRepo,
+            dns::dal::DeploymentRepository& drRepo,
+            dns::dal::AuditRepository& arRepo,
+            int iAuditRetentionDays);
   ~ApiServer();
 
   void registerRoutes();
   void start(int iPort, int iThreads);
   void stop();
+
+ private:
+  crow::SimpleApp _app;
+  dns::security::AuthService& _asService;
+  const dns::api::AuthMiddleware& _amMiddleware;
+  dns::dal::ProviderRepository& _prRepo;
+  dns::dal::ViewRepository& _vrRepo;
+  dns::dal::ZoneRepository& _zrRepo;
+  dns::dal::VariableRepository& _varRepo;
+  dns::dal::RecordRepository& _rrRepo;
+  dns::dal::DeploymentRepository& _drRepo;
+  dns::dal::AuditRepository& _arRepo;
+  int _iAuditRetentionDays;
 };
 
 }  // namespace dns::api
