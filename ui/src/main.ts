f33a2811f37aea06f5c8b6b9251802bb1a6ba5f6
Michael J. Hill <mjhill@mjhnosekai.com>
2026-03-15 12:49:57 -0400

    fix(ui): register PrimeVue Tooltip directive globally

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
 import ConfirmationService from 'primevue/confirmationservice'
 import ToastService from 'primevue/toastservice'
+import Tooltip from 'primevue/tooltip'
 import { themeConfig } from './theme'
 import router from './router'
 import App from './App.vue'
@@ -17,5 +18,6 @@ app.use(router)
 app.use(PrimeVue, { theme: themeConfig })
 app.use(ConfirmationService)
 app.use(ToastService)
+app.directive('tooltip', Tooltip)
 
 app.mount('#app')
